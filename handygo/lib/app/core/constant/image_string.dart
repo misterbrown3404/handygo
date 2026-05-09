459efba30eb58dbfdf52287c4cfc10652e61945c
Jibril Abdulsalam <abdulsalamjibril5@gmail.com>
2026-05-09 08:40:53 +0100

    Initial commit: Add GitHub Actions workflow for Flutter CI/CD

---
+++ handygo/lib/app/core/constant/image_string.dart
@@ -0,0 +1,54 @@
+class ImageStrings {
+  // Splash Screen
+  static const String appLogo = 'assets/splash/logo.png';
+
+  // Onboarding Screen
+  static const String onb1 = 'assets/onboarding/1.png';
+  static const String onb2 = 'assets/onboarding/2.png';
+  static const String onb3 = 'assets/onboarding/3.png';
+
+  // Auth Screen
+  static const String phone = 'assets/auth/phone.png';
+  static const String otp = 'assets/auth/otp.png';
+  static const String location = 'assets/auth/location.png';
+  static const String countryFlag = 'assets/auth/country_flag.png';
+
+  // Onboarding Language Icons
+  static const String englishLang = 'assets/onboarding/english.png';
+  static const String frenchLang = 'assets/onboarding/french.png';
+
+  // Categories Icons
+  static const String carWash = 'assets/category/car-wash.png';
+  static const String carRepair = 'assets/category/car-repair.png';
+  static const String carInspection = 'assets/category/car-inspection.png';
+  static const String carRental = 'assets/category/car-rental.png';
+
+  // Home Screen
+  static const String banner = 'assets/banner/banner.png';
+  static const String bgPattern = 'assets/home/bg-pattern.png';
+  static const String stars = 'assets/home/stars.png';
+  static const String currentLoc = 'assets/home/current-loc.png';
+
+  // Drawer
+  static const String sideMenu = 'assets/drawer/menu.png';
+  static const String profilePic = 'assets/drawer/profile.png';
+  static const String vehicle = 'assets/drawer/vehicle.png';
+  static const String myJobs = 'assets/drawer/my-jobs.png';
+  static const String notification = 'assets/drawer/notification.png';
+  static const String settings = 'assets/drawer/settings.png';
+  static const String logout = 'assets/drawer/logout.png';
+
+  // Language Icons
+  static const String arabicLang = 'assets/language/ar.png';
+  static const String englishLangFull = 'assets/language/en.png';
+  static const String frenchLangFull = 'assets/language/fr.png';
+
+  // Payment
+  static const String mastercard = 'assets/payment/mastercard.png';
+  static const String visa = 'assets/payment/visa.png';
+  static const String cash = 'assets/payment/cash.png';
+
+  // Providers Screen
+  static const String rateBar = 'assets/providers/rate_bar.png';
+  static const String map = 'assets/providers/map.png';
+}
